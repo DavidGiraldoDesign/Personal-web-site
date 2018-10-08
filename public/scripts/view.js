852b0e2ac6aa7d7cd237a5b07db7a12546e39c02
Jose David <josedavidgm1995@gmail.com>
2018-10-08 09:00:50 -0500

    Look and feel mobile

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -5,10 +5,11 @@ let view = {
     getMenuComponent: function(){
         let div = document.createElement('div');
         div.id='menuComponent';
-        div.className='row col-6 col-m-6 col-s-6';
+        div.className='row col-12 col-m-12 col-s-12';
         div.innerHTML=` 
-                        <!--div><h1><span id="int">{Interactive}</span><br> Product Designer</h1></div-->
-                        <div id="menuButtonsContainer"></div>
+                        <!--div id="logo">Logo</div-->
+                        <div  id="myPortfolio"><h1><span>{Interactive} </span> Product Designer</h1></div>
+                        <div  id="menuButtonsContainer"></div>
 
                         `;
         
